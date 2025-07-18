2b77752a32107815bf7ca6a719b445c768dc42ad
unknown <mohit.544509@gmail.com>
2025-07-18 16:51:18 +0530

    third commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { alertTitleClasses } from '../../../../../node_modules/@mui/material/index';
 import './style.css'
 
 console.log('Tailwind + TypeScript working!');
@@ -39,13 +40,98 @@ function closeModal() {
   }
 }
 
-function UserPage(){
-    alert("its working")
-    location.href='blog.html'
+function UserPage() {
+  const modal = document.getElementById("formModal");
+  if (modal) {
+    modal.classList.remove("hidden");
+  }
 }
+let ListView= true;
+function SwitchView() {
+
+  const cardText = document.getElementById("cardText");
+  const cardSection = document.getElementById("cardSection");
+  const listSection = document.getElementById("listSection");
+  const toggleBtn = document.getElementById("toggleBtn"); // Optional: for changing text
+  if(!toggleBtn){return}
+  
+  if (ListView) {
+    cardSection?.classList.add("hidden");
+    cardText?.classList.add("hidden");
+    listSection?.classList.remove("hidden");
+    
+    toggleBtn.innerText = "🧩 Show Card View";
+  } else {
+    cardSection?.classList.remove("hidden");
+    cardText?.classList.remove("hidden");
+    listSection?.classList.add("hidden");
+    toggleBtn.innerText = "📃 Show List View";
+  }
+
+  ListView = !ListView; // Toggle view
+}
+
+
+function closeFormModal() {
+  const modal2 = document.getElementById("formModal");
+  if (modal2) {
+    modal2.classList.add("hidden");
+  }
+}
+
+
+function Submit(event:Event){
+  event.preventDefault();
+  const AuthorName=(document.getElementById("authorName") as HTMLInputElement).value
+  const Place=(document.getElementById("placeName") as HTMLInputElement).value
+  const City=(document.getElementById("city") as HTMLInputElement).value
+  const State=(document.getElementById("state") as HTMLInputElement).value
+  const SType=(document.getElementById("spotType") as HTMLInputElement).value
+  const Image=(document.getElementById("imageUrl") as HTMLInputElement).value
+  const Short=(document.getElementById("shortDesc") as HTMLInputElement).value
+  const Detail=(document.getElementById("detailedDesc") as HTMLInputElement).value;
+  (document.getElementById("spotForm") as HTMLFormElement).reset();
+
+  const UserDetail = {
+   AuthorName,
+   Place,
+   City,
+   State,
+   SType,
+   Image,
+   Short,
+   Detail
+  }
+ 
+  const oldData= localStorage.getItem("userBlog");
+  let dataArray = [];
+  if(oldData){
+    dataArray =  JSON.parse(oldData);
+  }
+  dataArray.push(UserDetail);
+  localStorage. setItem("userBlog",JSON.stringify(dataArray))
+  display()
+}
+
+function display(){
+  const Data= localStorage.getItem("userBlog");
+  if(!Data){return}
+  const Convert= JSON.parse(Data);
+  console.log(Convert.AuthorName)
+}
+window.addEventListener("DOMContentLoaded", display);
+
+
+
+
+ 
+      
 
 // Make functions accessible in HTML inline onclick
 (window as any).showModal = showModal;
 (window as any).closeModal = closeModal;
 (window as any).DarkMode = DarkMode;
 (window as any).UserPage = UserPage;
+(window as any).SwitchView = SwitchView;
+(window as any).Submit = Submit;
+(window as any).closeFormModal = closeFormModal;
